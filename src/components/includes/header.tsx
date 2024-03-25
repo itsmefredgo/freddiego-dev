20e5b69efeee8da0d2073b957801c1244ff4d4b8
Frederick Go <dn282145@dal.ca>
2024-03-25 13:23:48 -0300

    fix: build fixed

--- src/components/includes/header.tsx
+++ src/components/includes/header.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
-import {links} from "../../../lib/data"
+import { links } from "../../../lib/data";
 
 function Header() {
   return (
     <header className=" h-10">
       <ul className=" flex gap-8">
         {links.map(({ name, address }, index) => (
-          <li>
-            <Link key={index} href={address}>
-              {name}
-            </Link>
+          <li key={index}>
+            <Link href={address}>{name}</Link>
           </li>
         ))}
       </ul>
