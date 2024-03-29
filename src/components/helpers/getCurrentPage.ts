0e50d8001a2e2858e8c5cd838fc7e8c7aa469e48
Frederick Go <dn282145@dal.ca>
2024-03-28 21:45:22 -0300

    fix: helper function export fixed from component to function

---
+++ src/components/helpers/getCurrentPage.ts
@@ -0,0 +1,13 @@
+import { usePathname } from "next/navigation";
+
+const getCurrentPage = (lang: string) => {
+  const path =
+    lang === "en"
+      ? usePathname().split("/")[1] // en
+      : usePathname().split("/").length === 2 // not en
+      ? "" // root
+      : usePathname().split("/")[2]; // not root
+  return path;
+};
+
+export default getCurrentPage;
