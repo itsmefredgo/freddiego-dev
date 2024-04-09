c4286d650ddecf18366888feb8dd530657b1a4f7
Frederick Go <dn282145@dal.ca>
2024-04-08 21:56:40 -0300

    update: main page edit started

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,9 +1,8 @@
-// export default function Home() {
-//   return <main>Contents</main>;
-// }
-
+import { Separator } from "@/src/components/ui/separator";
 import { Locale } from "@/src/i18n.config";
 import { getDictionary } from "@/lib/dictionary";
+import { AuroraBackground } from "@/src/components/ui/aurora-background";
+import { CiMapPin } from "react-icons/ci";
 
 export default async function Home({
   params: { lang },
@@ -13,12 +12,47 @@ export default async function Home({
   const { page } = await getDictionary(lang);
 
   return (
-    <section className=" ">
-      <div className="">
-        <h1 className="">{page.home.introduction.greeting}</h1>
-        <p className="">{page.home.introduction.name}</p>
-        <p className="">{page.home.introduction.role}</p>
-      </div>
-    </section>
+    <div className=" ">
+      <AuroraBackground className="w-full h-fit rounded-3xl mb-8 p-8 text-black dark:text-white">
+        <section className=" w-full bg-transparent rounded-3xl mb-8 p-8 z-10">
+          <div>
+            <h1 className=" text-[2.5rem]">
+              {page.home.introduction.greeting}
+            </h1>
+            <p className=" text-[2rem] text-primary">
+              {page.home.introduction.name}
+            </p>
+          </div>
+          <Separator className=" my-4" />
+          <div className="flex h-6 items-center space-x-4 text-sm">
+            <div className=" flex-1">
+              <p>Software Engineer</p>
+            </div>
+            <Separator orientation="vertical" className=" " />
+            <div className=" flex-1">
+              <p>Full-stack Developer </p>
+            </div>
+            <Separator orientation="vertical" className=" " />
+            <div className=" flex-1">
+              <p>Data Scientist </p>
+            </div>
+          </div>
+          <Separator className=" my-4" />
+          <div className="flex h-[2rem] items-center space-x-4 text-[1.5rem]">
+            <div className=" flex-1 flex gap-1">
+              <div className=" flex flex-col justify-center">
+                <CiMapPin className=" h-[2rem]" />
+              </div>
+              <p>Canada</p>
+            </div>
+            <Separator orientation="vertical" className=" " />
+            <div className=" flex-1">
+              <p>Nice to see you. </p>
+            </div>
+          </div>
+        </section>
+        <section className=""></section>
+      </AuroraBackground>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
+import type { Config } from "tailwindcss";
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         text: "var(--text)",
@@ -24,10 +32,15 @@ module.exports = {
         accent: "var(--accent)",
         subtext: "var(--subtext)",
       },
-      animation: {
-        shimmer: "shimmer 2s linear infinite",
-      },
       keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
         shimmer: {
           from: {
             backgroundPosition: "0 0",
@@ -36,11 +49,27 @@ module.exports = {
             backgroundPosition: "-200% 0",
           },
         },
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        shimmer: "shimmer 2s linear infinite",
+        aurora: "aurora 60s linear infinite",
       },
     },
   },
-  plugins: [addVariablesForColors],
-};
+  plugins: [addVariablesForColors, require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config;
 
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
